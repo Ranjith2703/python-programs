c4a28de48484307cb6994abc34b586ae8c5d918d
Ranjithkumar A <152938068+Ranjith2703@users.noreply.github.com>
2024-10-09 06:50:27 +0530

    Add files via upload

---
+++ opencv2.py
@@ -0,0 +1,89 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "mouse","bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+while True:
+    ret, img = cap.read()
+
+    # YOLO object detection
+    results = model(img, stream=True)
+
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            print("Confidence --->", confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    # Display video feed in the top-left corner
+    small_img = cv2.resize(img, (320, 240))
+    cv2.imshow('Webcam', small_img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+'''import cv2 as cv
+from sympy import capture'''
+
+'''
+img=cv.imread("matplotlib/carrot.jpg")
+cv.imshow("carrrot",img)
+
+cv.waitKey(0)'''
+
+'''cp=cv.VideoCapture("matplotlib\VID-20240113-WA0000.mp4")#here zero refers the activate the camera
+
+while True:
+    isTrue, frame=capture.read()
+    cv.show("video",frame)
+    if cv.waitKey(20)& 0xFF==ord("d"):
+        break
+
+capture.release()
+cv.destroyAllWindows()'''
